a58e6793050c11a25b075fda847e5a36b165cb28
wulala123wu <1105004820@qq.com>
2024-05-15 18:40:21 +0800

    main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import argparse
 import os
 
 SLEEPTIME = 0.2
-ENDTIME = "07:01:00"
+ENDTIME = "19:01:00"
 
 class reserve:
     def __init__(self):
@@ -53,9 +53,9 @@ class reserve:
         self.requests.get(url=self.login_page, verify=False)
 
     def get_submit(self, url, seat, token, roomid, seatid, captcha, action=False):
-        day = datetime.date.today() + datetime.timedelta(days=0)  # 预约今天，修改days=1表示预约明天
+        day = datetime.date.today() + datetime.timedelta(days=2)  # 预约今天，修改days=1表示预约明天
         if action:
-            day = datetime.date.today() + datetime.timedelta(days=1)  # 预约今天，修改days=1表示预约明天
+            day = datetime.date.today() + datetime.timedelta(days=2)  # 预约今天，修改days=1表示预约明天
         enc = encode(roomid, str(day), seat[0],seat[1],seatid,token)
         parm = {
             "roomId": roomid,
